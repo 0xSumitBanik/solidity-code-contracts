d3c5ca8129118bcba5a4659284c02d38e1cb1b53
Sumit Banik <sumit.banik>
2022-02-12 00:46:57 +0530

    Added First Contract
    Signed-off-by: Sumit Banik <sumit.banik>

---
+++ 01_FirstContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT 
+
+/* Note: 👆 The above line is mandatory to avoid compiler warnings */
+
+pragma solidity ^0.8.0;
+// pragma abicoder v1;
+
+contract FirstContract{
+    
+}
