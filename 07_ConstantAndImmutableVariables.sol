a4b875f25845606c64285432db50443cda1c1771
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-13 00:05:04 +0530

    [add] Constant and Immutable
    Signed-off-by: Sumit Banik <sumitbanik.02@gmail.com>

---
+++ 07_ConstantAndImmutableVariables.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract ConstantAndImmutable{
+   // State variables can be declared as: constant or immutable.
+
+   // For constant variables, the value has to be fixed at compile-time, 
+   // while for immutable, it can still be assigned at construction time. (inside constructor or declare at once).
+
+   uint constant COUNT = 123; // This variable cannot be re-initialized.
+   function InvalidOperation() external{
+      // COUNT  = 456; 👈 This will cause an error.
+   }
+
+   uint immutable IMMUT = 0;
+   constructor (uint _IMMUT) {
+        // IMMUT = _IMMUT; 👈 This is not allowed as IMMUT is already assigned a value.
+   }
+
+  //  This will work 👇
+  /*
+   uint immutable NOCHANGE;
+   constructor (uint _NOCHANGE){
+     NOCHANGE = _NOCHANGE;
+   }
+  */
+
+}
