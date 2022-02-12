f528e4096ec3dd7a547ef105c983dfe66e089ddc
Sumit Banik <sumit.banik>
2022-02-12 15:18:47 +0530

    Added Comments Explaining the Basic Contract

--- 01_FirstContract.sol
+++ 01_FirstContract.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT 
 
-/* Note: 👆 The above line is mandatory to avoid compiler warnings */
+/* Note: 👆 The above line is mandatory to avoid compiler warnings like 
+(Warning: SPDX license identifier not provided in source file.) */
 
-pragma solidity ^0.8.0;
-// pragma abicoder v1;
 
+/* pragma keyword is used to enable certain compiler features or checks. */
+pragma solidity ^0.8.0; // This is a version pragma
+/* The above version 👆 syntax denotes that the contract will not be compiled for version earlier
+than 0.8.0 and will compile on version less than the major version i.e. 0.9.0 */
+
+/* Contract 📃 is created using the contract keyword */
 contract FirstContract{
     
 }
