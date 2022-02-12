31cdb153418e26c804d5bbab45de1be1d798ad64
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-12 20:55:08 +0530

    [add] Solidity Functions Snippet
    Signed-off-by: Sumit Banik <sumitbanik.02@gmail.com>

---
+++ 03_FunctionsIntro.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract FunctionsIntro{
+
+    /* Note: All of the function visibility topics will be discussed later. */
+    // Addition
+    function add(uint x , uint y) external pure returns (uint){
+      return x+y;
+    }
+
+    // Subtraction
+    function sub(uint x , uint y) external pure returns (uint){
+      return x-y;
+    }
+   
+    // Multiplication
+    function mul(uint x , uint y) external pure returns (uint){
+      return x*y;
+    }
+    
+    // Division
+    function div(uint x , uint y) external pure returns (uint){
+      return x/y;
+    }
+
+    // Modulo
+    function mod(uint x , uint y) external pure returns (uint){
+      return x%y;
+    }
+
+    // Exponentiation
+    function exponentiation(uint x , uint y) external pure returns (uint){
+      return x**y;
+    }
+
+}
