9d79a59205e4e0951f1a7b44fb82137a27096a34
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-15 18:04:09 +0530

    [add] fixed size arrays

---
+++ 14_Arrays.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract CaptureFunctionOutputs{
+  /* Fixed Size Arrays */
+  uint constant SIZE = 5;
+  uint[SIZE] public x = [1, 2, 3, 4, 5];
+  //             index:  0  1  2  3  4
+  uint[SIZE] public y = [10, 20];
+  //             index: [0    1]  2  3  4 -> these indexes are initialized with 0
+
+}
