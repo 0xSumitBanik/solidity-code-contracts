53cbdec88440cfc9be812d6b88223444be08ea5c
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-15 10:56:20 +0530

    [add] constructor code

---
+++ 12_Constructor.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract Constructor{
+  address public owner;
+  uint public balance;
+
+  constructor(uint _balance){
+    // Set the owner to the address that has deployed the contract
+    owner = msg.sender;
+    balance = _balance;
+  }
+}
