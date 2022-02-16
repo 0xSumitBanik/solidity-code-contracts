5065710734f7855288857e57891365111c9161f2
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-17 00:44:47 +0530

    [add] nested mapping

--- 15_Mapping.sol
+++ 15_Mapping.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 contract Mapping{
   // Syntax: mapping(_KeyType => _ValueType)
   mapping(address => uint) public balances;
+  mapping(address => mapping (address=>bool)) public relational;
 
   function mappingOperations() external{
     balances[msg.sender] = 1000; // set the value for msg.sender to 1000
