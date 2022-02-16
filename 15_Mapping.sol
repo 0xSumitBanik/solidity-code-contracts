e3c50e030bafa3da5ae345504c4c1092d14ac143
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-17 00:11:36 +0530

    [add] mapping operations

--- 15_Mapping.sol
+++ 15_Mapping.sol
@@ -5,5 +5,10 @@ pragma solidity ^0.8.0;
 contract Mapping{
   // Syntax: mapping(_KeyType => _ValueType)
   mapping(address => uint) public balances;
-  
+
+  function mappingOperations() external{
+    balances[msg.sender] = 1000; // set the value for msg.sender to 1000
+
+    delete balances[msg.sender]; // Sets it to false value (here, 0) for specified key
+  }
 }
