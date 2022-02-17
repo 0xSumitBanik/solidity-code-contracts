fc14dfe800d314473a2093377fa7344729fe2d20
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-18 00:36:25 +0530

    [add] Iterable Mapping Functions

--- 16_IterableMapping.sol
+++ 16_IterableMapping.sol
@@ -3,5 +3,24 @@
 pragma solidity ^0.8.0; 
 
 contract IterableMapping{
+  mapping(address=>uint) public balances;
+  mapping(address=>bool) public inserted;
+  address[] public mappingKeys;
 
+  function setValue(address _key, uint _balance) external{
+    balances[_key]=_balance;
+
+    if(!inserted[_key]){
+      inserted[_key]=true;
+      mappingKeys.push(_key);
+    }
+  }
+
+  function getSize() external view returns (uint){
+    return mappingKeys.length;
+  }
+
+  function getValue(uint _index) external view returns (uint){
+    return balances[mappingKeys[_index]];
+  }
 }
