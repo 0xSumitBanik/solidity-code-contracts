9fc169e3a2540df30e23d1be1fcdef78b82c6ddc
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-19 00:20:34 +0530

    [add] structs in solidity

--- 17_Structures.sol
+++ 17_Structures.sol
@@ -3,5 +3,24 @@
 pragma solidity ^0.8.0; 
 
 contract Structures{
+  // Struct types are used to represent a record.
+  struct Book{
+    uint bookId;
+    string name;
+    string author;
+  }
+  Book public book; // book is the structure state variable name
+
+  // How to set values for struct members (Using . operator)
+  function setBookVariable() external{
+    book.bookId  = 1;
+    book.name    = "Solidity Doc";
+    book.author  = "Dev XYZ";
+  }
+
+  // How to set values for struct members (Wrapping the values in struct() )
+  function setBookVariable1() external{
+    book = Book(1,"ABC","DEF");
+  }
 
 }
