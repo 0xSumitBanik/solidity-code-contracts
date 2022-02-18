29b55d162ce1fb6a7bedbfb6ca309a4e04821365
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-18 15:37:01 +0530

    [add] Comments about Iterable Mapping

--- 16_IterableMapping.sol
+++ 16_IterableMapping.sol
@@ -3,10 +3,14 @@
 pragma solidity ^0.8.0; 
 
 contract IterableMapping{
+  // Mapping for storing the balances for specific address
   mapping(address=>uint) public balances;
+  // Mapping to store and keep track of inserted addresses
   mapping(address=>bool) public inserted;
+  // Dynamic Array to store the mapping keys (here: inserted addresses)
   address[] public mappingKeys;
 
+  // Set the value for the inserted key and push it to the array of keys if reqd.
   function setValue(address _key, uint _balance) external{
     balances[_key]=_balance;
 
@@ -16,10 +20,12 @@ contract IterableMapping{
     }
   }
 
+  // Implemented the function of getting the mapping size
   function getSize() external view returns (uint){
     return mappingKeys.length;
   }
 
+  // Get the value using the mappingKeys Array Index
   function getValue(uint _index) external view returns (uint){
     return balances[mappingKeys[_index]];
   }
