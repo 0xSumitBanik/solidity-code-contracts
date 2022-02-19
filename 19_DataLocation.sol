b8bc2cac732bd7c06fa3b8e913e909043b18b575
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-19 20:47:30 +0530

    [add] Data Location Types

---
+++ 19_DataLocation.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract DataLocation{
+  /* Data Locations:
+      - storage : Location type for storing state variables which means data will be persisted.
+      - memory : 
+      - calldata
+  */
+}
