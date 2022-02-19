26c999d8d87ac4246b4f9389482fab2039ae6477
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-19 14:16:04 +0530

    [note] about enums in solidity

--- 18_Enums.sol
+++ 18_Enums.sol
@@ -3,5 +3,9 @@
 pragma solidity ^0.8.0; 
 
 contract Enums{
+  // Enums restrict a variable to have one of only a few predefined values. 
+  // The values in this enumerated list are called enums.
 
+  // Enums require at least one member, and its default value when declared is the first member. 
+  // Enums cannot have more than 256 members.
 }
