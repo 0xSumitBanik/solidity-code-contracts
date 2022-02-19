8bda3154804ceeee6e2919b4690237b0aa92ef66
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-20 00:00:32 +0530

    [add] memory data location

--- 19_DataLocation.sol
+++ 19_DataLocation.sol
@@ -4,8 +4,26 @@ pragma solidity ^0.8.0;
 
 contract DataLocation{
   /* Data Locations:
+    When we use a Reference data type, we also need to specify the data location. 👇
+                ( ☝ structs, arrays and mapping )
       - storage : Location type for storing state variables which means data will be persisted.
-      - memory : 
-      - calldata
+      - memory :  To hold the value onto memory.
+      - calldata : Similiar to memory but this can only be used with function inputs.
   */
+
+  struct Asset{
+    uint assetId;
+    string name; 
+  }
+
+  Asset public asset; // Stored in the storage
+
+  function operation() external returns (Asset memory){
+  // Asset is a struct that's why we are using memory ☝
+     asset.assetId=1;
+     asset.name="ETH Documentation";
+
+     return asset;
+  }
+
 }
