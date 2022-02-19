8708ac321a75ad34f7cccbdafcc4083d6130f81d
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-19 13:55:42 +0530

    [add] Array of Struct variables

--- 17_Structures.sol
+++ 17_Structures.sol
@@ -10,6 +10,7 @@ contract Structures{
     string author;
   }
   Book public book; // book is the structure state variable name
+  Book[] public books; // Array of struct
 
   // How to set values for struct members (Using . operator)
   function setBookVariable() external{
@@ -19,9 +20,19 @@ contract Structures{
   }
 
   // How to set values for struct members (Wrapping the values in struct() )
-  function setBookVariable1() external{
+  function setBookVariable1() external {
     book = Book(1,"ABC","DEF"); // Option-1 (order of values must be same as defined in struct)
-    book = Book({name:"ABC",bookId:1,author:"DEF"}); // Option-2 (specify key names to follow any order)
+    book = Book({name:"ABfC",bookId:1,author:"DEF"}); // Option-2 (specify key names to follow any order)
   }
 
+  function structMemoryVars() external{
+    // 
+    Book memory b1 = Book(10,"XYZ-1","xyz-1");
+    Book memory b2 = Book(100,"XYZ-2","xyz-2");
+
+    // Push the structure memory variable to the Books[]
+    books.push(b1);
+    books.push(b2);
+
+  }
 }
