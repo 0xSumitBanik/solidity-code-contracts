2e4a3c1cd8825daefa27468ad4ea89801137b399
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-22 00:19:02 +0530

    [add] broadcast message event in solidity

--- 20_Events.sol
+++ 20_Events.sol
@@ -2,7 +2,15 @@
 
 pragma solidity ^0.8.0; 
 
-
+/*
+  Note: 
+  - Solidity events give an abstraction on top of the EVM’s logging functionality. 
+  - Applications can subscribe and listen to these events through the RPC interface of an Ethereum client.
+*/
 contract Events{
+  event BroadcastMessageEvent(address indexed brodcaster, address indexed receiver,string indexed message);
 
+  function sendMessage(address _receiver, string calldata _message) external{
+      emit BroadcastMessageEvent(msg.sender, _receiver, _message);
+  }
 }
