34ebf3bc08d52794ecea1df2909952dec05b2e91
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-22 11:00:37 +0530

    [add] note about events

--- 20_Events.sol
+++ 20_Events.sol
@@ -8,8 +8,16 @@ pragma solidity ^0.8.0;
   - Applications can subscribe and listen to these events through the RPC interface of an Ethereum client.
 */
 contract Events{
-  event BroadcastMessageEvent(address indexed brodcaster, address indexed receiver,string indexed message);
+  // indexed is used to filter events by parameter.
+  // Up to three paramters can be indexed.
+  // It adds them to a special data structure known as “topics” instead of the data part of the log.
 
+  event BroadcastMessageEvent(address indexed brodcaster, address indexed receiver,string indexed message);
+  
+  // We can declare the event as anonymous by adding the keyword anonymous after the Event() 
+  // event BroadcastMessageEvent(address indexed brodcaster, address indexed receiver,string indexed message) anonymous;
+
+  // We are writing to the blockchain by logging the data that makes it a transactional function rather than view/pure.
   function sendMessage(address _receiver, string calldata _message) external{
       emit BroadcastMessageEvent(msg.sender, _receiver, _message);
   }
