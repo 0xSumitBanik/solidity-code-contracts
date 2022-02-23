145354820476280311efaa51655051fb903c480a
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-24 00:03:56 +0530

    [add] comment about virtual keyword

--- 21_SingleInheritance.sol
+++ 21_SingleInheritance.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0; 
 
 contract ParentContract{
+  // We add virtual keyword to indicate that this function can be overriden by the child contract
   function displayName() external virtual pure returns (string memory){
       return "This is Parent Contract";
   }
