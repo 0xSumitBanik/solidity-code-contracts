e060f0b0756dfd80fda88a514d6ff06c9e6f5159
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-28 15:37:04 +0530

    [add] note about single inheritance

--- 21_SingleInheritance.sol
+++ 21_SingleInheritance.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT 
 
 pragma solidity ^0.8.0; 
+/*
+Single Inheritance:
 
+In Single or single level inheritance the functions and variables of one base contract are inherited to only one derived contract.
+*/
 contract ParentContract{
   // We add virtual keyword to indicate that this function can be overriden by the child contract
   function displayName() external virtual pure returns (string memory){
