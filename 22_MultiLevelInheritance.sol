bcdebc1f8477a97c40f4206a7fca32550889ea99
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-28 15:36:18 +0530

    [add] note about Multi Level Inheritance

--- 22_MultiLevelInheritance.sol
+++ 22_MultiLevelInheritance.sol
@@ -2,6 +2,14 @@
 
 pragma solidity ^0.8.0; 
 
+/*
+Multi-level Inheritance:
+
+It is very similar to single inheritance, but the difference is that 
+it has levels of the relationship between the parent and the child. 
+The child contract derived from a parent also acts as a parent for the contract which is derived from it.
+*/
+
 contract ParentContract{
   // We add virtual keyword to indicate that this function can be overriden by the child contract
   function displayName() external virtual pure returns (string memory){
