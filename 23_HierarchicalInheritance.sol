d03bddd94020a4a56c521466d17f191ffb4366f9
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-01 12:40:07 +0530

    [add] Hierarchical Inheritance in Solidity

---
+++ 23_HierarchicalInheritance.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+/*
+Multi-level Inheritance:
+
+It is very similar to single inheritance, but the difference is that 
+it has levels of the relationship between the parent and the child. 
+The child contract derived from a parent also acts as a parent for the contract which is derived from it.
+*/
+
+contract ParentContract{
+  // We add virtual keyword to indicate that this function can be overriden by the child contract
+  function displayName() external virtual pure returns (string memory){
+      return "This is Parent Contract";
+  }
+}
+
+
+contract FirstChildContract is ParentContract{
+  // As this function will also be overriden by another child contract hence it is set to virtual and override
+  function displayName() external virtual override pure returns (string memory){
+      return "This is First Child Contract";
+  }
+}
+contract SecondChildContract is ParentContract{
+  // The following function is overriden with override keyword
+  function displayName() external override pure returns (string memory){
+      return "This is Second Child Contract";
+  }  
+}
