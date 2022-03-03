c9d0210d7780b731c86b504a693566bda8314353
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-03 16:10:34 +0530

    [add] demonstration of multiple inheritance

--- 24_MultipleInheritance.sol
+++ 24_MultipleInheritance.sol
@@ -11,12 +11,53 @@ A parent contract can have more than one child while a child contract can have m
 
 contract FirstParentContract{
   // We add virtual keyword to indicate that this function can be overriden by the child contract
-  function displayName() external virtual pure returns (string memory){
-      return "This is First Parent Contract";
+  function greeter() external virtual pure returns (string memory){
+      return "Hello, I am First Parent Contract";
   }
 }
 
 contract SecondParentContract{
-
+  event Logging(string message);
 }
  
+contract FirstChild is SecondParentContract{
+  function displayHello() external returns(string memory){
+    emit Logging("I am first child");
+    return "Hello";
+  }
+}
+
+contract SecondChild is SecondParentContract{
+  string public childName; // stores the name
+  constructor(){
+    childName = "Second Child";
+  }
+}
+
+// Note: The Inheritance Order must have the most base like contract name first
+// followed by derived like.
+// Here, the most base like is SPC (as it has no other inherited contracts)
+// contract FirstGrandChild is SecondChild , SecondParentContract. (This will give error) ❌
+
+contract FirstGrandChild is SecondParentContract,SecondChild{
+  constructor(){
+    childName = "First Grand Child";
+  }
+
+  function hello() external returns(string memory){
+      emit Logging("I am first grand child");
+      return "Hello";
+  }
+}
+
+
+/* Note:
+
+  1. Order of Inheritance
+          FPC      SPC
+                ↗   |  ↖
+              FC   |   SC
+                  FGC
+    
+    where FPC: First Parent Contract , and so on.
+*/
