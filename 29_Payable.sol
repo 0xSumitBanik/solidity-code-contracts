8aa9605969b9fd68f65f6524866625272260f679
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-05 21:05:47 +0530

    [add] payable function in solidity

---
+++ 29_Payable.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract Payable{
+  // payable is a modifier that can be added to a function.
+  // It allows a function to receive ether while being called.
+
+  function depositETH() external payable{
+
+  }
+
+  // This function returns the balance (in Wei) of the specified address.
+  // To Check if the depositETH() has worked properly, we'll pass the deployed smart contract address.
+  function getBalance(address _addr) external view returns(uint){
+    return _addr.balance;
+  }
+}
