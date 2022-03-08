6d38af98c6a8330c86f6e604759c85862b268eb2
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-09 00:58:50 +0530

    [add] send ether solidity example

--- 31_SendEther.sol
+++ 31_SendEther.sol
@@ -10,22 +10,39 @@ pragma solidity ^0.8.0;
   3. call (forward all gas or set gas, returns bool status)
 */
 contract SendEther{
-  // To enable the contract to receive ether, we have to make the constructor payable.
-  // or, include receive/fallback functions.
-
-  receive() payable external{}
+  // To enable the contract to store ether while deploying, we have to make the constructor payable.
+  constructor() payable{}
 
   function transferExample(address payable _receiver) external payable{
-    // This will transfer 1 wei.
-      _receiver.transfer(1);
+    // This will transfer the amount: number of wei sent with the message
+      _receiver.transfer(msg.value);
   }
 
   function sendExample(address payable _receiver) external payable{
-
+    // This will transfer the amount: number of wei sent with the message
+      bool sentStatus = _receiver.send(msg.value);
+      require(sentStatus,"Fund Transfer Failed");
   }
 
+  // This is the recommended method (more to be discussed later)
   function callExample(address payable _receiver) external payable{
-
+    // This will transfer the amount: number of wei sent with the message
+      (bool sentStatus,) = _receiver.call{value: msg.value}("");
+      require(sentStatus,"Fund Transfer Failed");
   }  
 
+}
+
+contract ReceiveEther{
+  event Log(address sender,uint amount,uint gas);
+
+  receive() external payable{
+    // gasleft() is a function which exists in global namespace.
+    // It returns the remaining gas.
+    emit Log(msg.sender,msg.value,gasleft());
+  }
+
+  function getBalance() external view returns(uint){
+    return address(this).balance;
+  }
 }
