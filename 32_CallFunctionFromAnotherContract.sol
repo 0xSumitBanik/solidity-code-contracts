dd856f46cdd890e79203b200c43415fdf00566db
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-18 01:12:31 +0530

    [add] note about the abi.encodeWithSignature

--- 32_CallFunctionFromAnotherContract.sol
+++ 32_CallFunctionFromAnotherContract.sol
@@ -19,6 +19,7 @@ contract CallingContract{
 
   function executeDisplayTextUsingCall(address _contractAddress, string calldata _text) external
   returns(bool,bytes memory){
+    // It returns two output: 1. status of call method, 2. bytes output of abi.encodeWithSignature function (discussed in details later)
     (bool _status, bytes memory _output)= _contractAddress.call(abi.encodeWithSignature("displayText(string)",_text));
     return(_status,_output);
   }
