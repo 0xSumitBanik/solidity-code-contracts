4c99d70056e9dbcb0298adf34914d0b90d19b3f6
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-18 11:38:35 +0530

    [add] delegate call example in solidity

---
+++ 35_DelegateCall.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0; 
+
+contract EmployeeRecord{
+
+  uint id;
+  mapping(uint=>string) public EmployeeMapping;
+
+  event Log(uint indexed,string);
+
+  function addEmployeeDetails(string calldata _name) external{
+    EmployeeMapping[id++]=_name;
+    emit Log(id,_name);
+  }
+
+}
+
+/* Note:
+  - There exists a special variant of a message call, named delegatecall 
+    which is identical to a message call apart from the fact 
+    that the code at the target address is executed in the context of the calling contract 
+    and msg.sender and msg.value do not change their values.
+*/
+
+
+// We will call this contract using the delegatecall
+contract EmployeeDB{
+
+  // Please maintain the order of state variables for which the delegatecall will be performed.
+  // Here, we are only using one State Variable.
+  uint id;
+
+
+  function delegateCallExample(address _contractAddress,string calldata _name) external{
+    (bool status,) = _contractAddress.delegatecall(
+      abi.encodeWithSelector(EmployeeRecord.addEmployeeDetails.selector, _name)
+    );
+    require(status,"Delegate Call Failed");
+  }
+
+}
