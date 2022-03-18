bcbd98476925443eb2ef47efa0699a2d141928c3
Sumit Banik <sumitbanik.02@gmail.com>
2022-03-18 15:26:27 +0530

    [note] added note about delegate call

--- 35_DelegateCall.sol
+++ 35_DelegateCall.sol
@@ -21,6 +21,10 @@ contract EmployeeRecord{
     which is identical to a message call apart from the fact 
     that the code at the target address is executed in the context of the calling contract 
     and msg.sender and msg.value do not change their values.
+
+  - This means that a contract can dynamically load code from a different address at runtime. 
+    Storage, current address and balance still refer to the calling contract, 
+    only the code is taken from the called address.
 */
 
 
